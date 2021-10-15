cd8d0a7e6b4536da9fe8379d357c894e9692cf66
Gabriel <naocensurado@yahoo.com.br>
2021-10-15 02:50:56 -0300

    Fixed the remove button, improved some comments

--- scripts/application.js
+++ scripts/application.js
@@ -1,62 +1,85 @@
 //Main code for the app.
-//I'll write other files if it get too big.
 
 //All books will be stored, as objects, inside a single array
 let myLibrary = [];
 
 //Function constructor for 'Book' objects
 function Book(title, author, pages, read) {
-    this.title = title
+    this.title = "\"" + title + "\"";
     this.author = author
     this.pages = pages
     this.read = read    
 }
-
 Book.prototype.showInfo = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
 
-Object.defineProperty(Book.prototype, "showInfo", {enumerable:false});
-
-//To put books into the library
 function addToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
+function removeFromLibrary(e) {
+    let bookIndex = +e.target.parentElement.dataset.index; //Data attribute created in following function
+    myLibrary.splice(bookIndex, 1);
+    displayBooks(true);
+}
 
-const bookCards = document.getElementsByClassName("books");
 //To display saved books
 //Takes one argument, indicating to show all the library or to just update (skipping the loop)
-function displayBooks(update) {    
+//Creates node(s) for the info, the text node from Book.prototype and button(s) node(s)
+const bookCards = document.getElementsByClassName("books"); //Stores the display containter
+function displayBooks(allContent) {    
     let bookCard;
     let bookInfo;
-    if (!update){
-        myLibrary.forEach(function (book) {
+    let deleteBook;
+    if (allContent){
+        while (bookCards[0].firstChild) {
+            bookCards[0].removeChild(bookCards[0].firstChild); //Will redo all the display
+        }
+        myLibrary.forEach(function (book) {            
             bookCard = document.createElement("div");
             bookCard.classList.add("book-card");
+            bookCard.setAttribute("data-index", myLibrary.indexOf(book)); //To identify books in the array
             bookInfo = document.createTextNode(book.showInfo());
+            deleteBook = document.createElement("button")
+            deleteBook.type = "button";
+            deleteBook.appendChild(document.createTextNode("Delete"));
             bookCard.appendChild(bookInfo);         
+            bookCard.appendChild(deleteBook);
             bookCards[0].appendChild(bookCard);
         })
     } else {
         bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
+        bookCard.setAttribute("data-index", (myLibrary.length - 1));
         bookInfo = document.createTextNode(myLibrary[myLibrary.length - 1].showInfo());
+        deleteBook = document.createElement("button")
+        deleteBook.type = "button";
+        deleteBook.appendChild(document.createTextNode("Remove"));
         bookCard.appendChild(bookInfo);         
+        bookCard.appendChild(deleteBook);                
         bookCards[0].appendChild(bookCard);
     }    
 }
 
-
+//The two following handlers purpose is to create a 'show/hide' function
+//for the formulary
 document.getElementById("open-form").addEventListener("click", () => document.getElementById("add-book").style.display = "block");
+document.getElementById("close-form").addEventListener("click", () => document.getElementById("add-book").style.display = "none");
+
+//'Add' button
 document.getElementById("book-form").onsubmit = (e) => {
     e.preventDefault();
     let fields = document.querySelectorAll("input");
     addToLibrary(fields[0].value, fields[1].value, fields[2].value, fields[3].value); 
-    displayBooks(true);
+    displayBooks(false);
 }
-document.getElementById("close-form").addEventListener("click", () => document.getElementById("add-book").style.display = "none");
+
+//'Remove' button
+bookCards[0].addEventListener("click", (e) => {
+    if (e.target.type = "button") removeFromLibrary(e);
+})
 
 
 
