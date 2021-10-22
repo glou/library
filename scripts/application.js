39224ef1df839a87698beb00451a4873d808e802
Gabriel <naocensurado@yahoo.com.br>
2021-10-22 16:12:28 -0300

    Made a working slider for books status

--- scripts/application.js
+++ scripts/application.js
@@ -7,33 +7,44 @@ let myLibrary = [];
 
 //Function constructor for 'Book' objects
 function Book(title, author, pages, read) {
-    this.title = "\"" + title + "\"";
-    this.author = author
-    this.pages = pages
-    this.read = read    
+    this.title = "\"" + title + "\"",
+    this.author = author,
+    this.pages = pages,
+    this.read = read 
 }
 Book.prototype.showInfo = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${(Boolean(+this.read) ? 'Read' : 'Not read')}`;
 };
 
 function addToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+    myLibrary.push(new Book(title, author, pages, read));    
 }
 
 function removeFromLibrary(e) {
-    let bookIndex = +e.target.parentElement.dataset.index; //Data attribute created in the following function
+    let bookIndex = +e.target.parentElement.dataset.index;
     myLibrary.splice(bookIndex, 1);
     displayBooks(true);
 }
 
+//Updates read/not read status inside the object and in display
+function updateStatus(e) {
+    let bookCard = e.target.parentElement;
+    let bookIndex = +bookCard.dataset.index; //Data attribute created in the following function
+    myLibrary[bookIndex].read = e.target.value;
+    let textNode = bookCard.firstChild;    
+    while (textNode.nodeType != Node.TEXT_NODE) {
+        textNode = textNode.nextSibling;        
+    }
+    textNode.textContent = myLibrary[bookIndex].showInfo();
+}
+
 //To display saved books
-//Takes one argument, indicating to show all the library or to just update (skipping the loop)
-//Creates node(s) for the info, the text node from Book.prototype and button(s) node(s)
+//Takes one argument, indicating to show all the library or to just show the added book (skipping the loop)
 const bookCards = document.getElementsByClassName("books"); //Stores the display containter
 function displayBooks(allContent) {    
     let bookCard;
     let bookInfo;
+    let statusBook;
     let deleteBook;
     if (allContent){
         while (bookCards[0].firstChild) {
@@ -43,11 +54,18 @@ function displayBooks(allContent) {
             bookCard = document.createElement("div");
             bookCard.classList.add("book-card");
             bookCard.setAttribute("data-index", myLibrary.indexOf(book)); //To identify books in the array
-            bookInfo = document.createTextNode(book.showInfo());
+            bookInfo = document.createTextNode(book.showInfo());            
+            statusBook = document.createElement("input");
+            statusBook.type = "range";
+            statusBook.min = "0";
+            statusBook.max = "1";
+            statusBook.value = book.read;
+            statusBook.appendChild(document.createTextNode("Have you read it?"));
             deleteBook = document.createElement("button")
             deleteBook.type = "button";
             deleteBook.appendChild(document.createTextNode("Delete"));
-            bookCard.appendChild(bookInfo);         
+            bookCard.appendChild(bookInfo);
+            bookCard.appendChild(statusBook);
             bookCard.appendChild(deleteBook);
             bookCards[0].appendChild(bookCard);
         })
@@ -55,11 +73,18 @@ function displayBooks(allContent) {
         bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
         bookCard.setAttribute("data-index", (myLibrary.length - 1));
-        bookInfo = document.createTextNode(myLibrary[myLibrary.length - 1].showInfo());
+        bookInfo = document.createTextNode(myLibrary[myLibrary.length - 1].showInfo());        
+        statusBook = document.createElement("input");
+        statusBook.type = "range";
+        statusBook.min = "0";
+        statusBook.max = "1";
+        statusBook.value = myLibrary[myLibrary.length - 1].read;
+        statusBook.appendChild(document.createTextNode("Have you read it?"));
         deleteBook = document.createElement("button")
         deleteBook.type = "button";
         deleteBook.appendChild(document.createTextNode("Remove"));
-        bookCard.appendChild(bookInfo);         
+        bookCard.appendChild(bookInfo);
+        bookCard.appendChild(statusBook);         
         bookCard.appendChild(deleteBook);                
         bookCards[0].appendChild(bookCard);
     }    
@@ -94,7 +119,12 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
 
 //'Remove' button
 bookCards[0].addEventListener("click", (e) => {
-    if (e.target.type = "button") removeFromLibrary(e);
+    if (e.target.type === "button") removeFromLibrary(e);
+})
+
+//'Read' slider
+bookCards[0].addEventListener("click", (e) => {
+    if (e.target.type === "range") updateStatus(e);
 })
 
 
